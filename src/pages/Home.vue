53a25cf44491bece9a0a15e919908e3895e4a431
longplayer <bartaber@hotmail.com>
2021-07-11 12:24:07 +0200

    Removed HelloWrold from homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="home">
     <SvgAnimationCat />
-    <HelloWorld msg="Vue 3 + Vite + TaliwindCSS" />
+    <a target="_blank" href="https://monsieurchat.fr/">Visit Artist website</a>
   </div>
 </template>
 
 <script>
-import HelloWorld from '@components/HelloWorld.vue'
 import SvgAnimationCat from '@components/SvgAnimationCat.vue'
 export default {
   components: {
-    HelloWorld,
     SvgAnimationCat,
   },
 }
