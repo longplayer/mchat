35b8403c9d5c071c62275c9aa9ac06d148938129
longplayer <bartaber@hotmail.com>
2021-07-14 18:25:02 +0200

    Auto register Base* components - used Vite way

--- src/main.js
+++ src/main.js
@@ -4,4 +4,18 @@ import router from './router'
 import store from './store'
 import './index.css'
 
-createApp(App).use(router).use(store).mount('#app')
+const app = createApp(App)
+
+// Classic Vue3 way will not work, use Vite way:
+// https://vitejs.dev/guide/features.html#glob-import
+// https://dev.to/jakedohm_34/auto-registering-all-your-components-in-vue-3-with-vite-4884
+const components = import.meta.globEager('./components/Base*.vue')
+Object.entries(components).forEach(([path, definition]) => {
+  // Get name of component, based on filename
+  const componentName = path.split('/').pop().replace(/\.\w+$/, '')
+
+  // Register component on this Vue instance
+  app.component(componentName, definition.default)
+})
+
+app.use(router).use(store).mount('#app')
