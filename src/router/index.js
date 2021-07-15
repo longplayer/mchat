44d4d4e55b5b20624c09438dd0fd067901e30af9
longplayer <bartaber@hotmail.com>
2021-07-15 17:03:40 +0200

    Added custom meta title

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store/index'
+import { applyMetaTitleSimple } from './metaguards'
 import Home from '@pages/Home.vue'
 import Playground from '@pages/Playground.vue'
 
+// todo: meta.title not work - fix this
 const routes = [
   {
     path: '/',
     name: 'home',
     component: Home,
+    meta: { title: store.state.appName }
   },
   {
     path: '/testycool',
     name: 'playground',
     component: Playground,
+    meta: { title: 'Playground a.k.a. the test page' },
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+// Insert meta.title to the page title
+router.beforeEach((to, from, next) => {
+  applyMetaTitleSimple(to, from, next)
 })
 
 export default router
