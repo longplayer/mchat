bc5871c9471fc7d2cb904279f1651cd40c453122
longplayer <bartaber@hotmail.com>
2021-08-03 13:59:41 +0200

    Added diagonale layout to the separator

--- src/components/SeparatorImage.vue
+++ src/components/SeparatorImage.vue
@@ -1,13 +1,25 @@
 <template>
-  <div
-    :class="[variantClass, placementClass]">
-    <div class="rationer">
-      <figure class="rationer-inside">
-        <img
-          :src="image"
-          alt="">
-        </figure>
-    </div>
+  <div :class="[variantClass, placementClass]">
+
+    <template v-if="placement === 'fullwidth'">
+      <div class="rationer">
+        <figure class="rationer-inside">
+          <img
+            :src="image"
+            alt="">
+          </figure>
+      </div>
+    </template>
+    <template v-if="placement === 'diagonale'">
+      <div class="shape-wrapper">
+        <div id="rhombus">
+          <div class="shape"></div>
+          <div class="content">
+            <img :src="image">
+          </div>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -49,23 +61,99 @@ export default {
 </script>
 
 <style lang="postcss">
-  .separator__image {
+.separator__image {
+
+  /* thematic rules */
+  &.separator__primary.separator__fullwidth {
+    @apply border-primary;
     border-width: 1rem 0;
 
     @screen lg { border-width: 2rem 0; }
     @screen xl { border-width: 3rem 0; }
+  }
+  &.separator__secondary.separator__fullwidth {
+    @apply border-secondary;
+  }
 
-    &.separator__primary {
-      @apply border-primary;
+  &.separator__primary.separator__diagonale {
+    .shape {
+      @apply text-primary;
     }
-    &.separator__secondary {
-      @apply border-secondary;
+  }
+  &.separator__secondary.separator__diagonale {
+    .shape {
+      @apply text-secondary;
     }
+  }
 
-    & img {
+  /* common rules */
+  &.separator__fullwidth {
+    
+    img {
       width: 100%;
       height: 100%;
       object-fit: cover;
     }
   }
+
+  &.separator__diagonale {
+
+    /* @apply bg-green-100; */
+
+    .shape-wrapper {
+
+      box-sizing: border-box;
+      width: 80vh;
+      padding: 0;
+      background-color: inherit;
+      position: relative;
+      margin: 10vh auto;
+    
+      #rhombus {
+
+        width: 100%;
+        background-color: inherit;
+        
+        .shape {
+          width: 0;
+          height: 0;
+          border: 40vh solid transparent;
+          border-bottom-color: currentColor;
+          position: relative;
+          top: -40vh;
+          
+          &:after {
+            content: "";
+            position: absolute;
+            left: -40vh;
+            top: 40vh;
+            width: 0;
+            height: 0;
+            border: 40vh solid transparent;
+            border-top-color: currentColor;
+          }
+        }
+        
+        .content {
+          display: block;
+          position: absolute;
+          overflow: hidden;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background: transparent;
+          
+          img {
+            position: relative;
+            width: 80%;
+            height: 80%;
+            transform: translate(12.5%, 12.5%);
+            clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+          }
+        }
+      }
+    }
+  }
+}
 </style>
