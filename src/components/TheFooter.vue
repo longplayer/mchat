b51f0a63feadc0a2b1633fefba4d82da693a9c56
longplayer <bartaber@hotmail.com>
2022-01-20 23:00:35 +0100

    Footer is now responsive

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -33,14 +33,15 @@ export default {
 
 <style lang="postcss" scoped>
 footer {
-  @apply relative bottom-0 flex w-full h-32 p-8 pr-36 bg-primary border-t-4 border-secondary;
+  @apply flex relative bg-primary border-t-4 border-secondary 
+    bottom-0 p-8 pt-16 md:pt-8 md:pl-36 w-full h-auto md:h-32 ;
 
   & li {
     @apply inline-block;
   }
 
   & a {
-    @apply inline-block px-4;
+    @apply inline-block px-0 sm:px-4;
 
     &:hover {
       @apply font-bold underline;
@@ -48,17 +49,43 @@ footer {
   }
 
   & .cat-container {
+    transform: translate(-1rem, -50%);
     @apply absolute top-0 right-0;
-    transform: translate(-1rem, -50%)
+    @screen sm {
+      left: 0;
+      right: inherit;
+      transform: translate(1rem, -50%);
+    }
   }
 
-  & .footer-navigation,
-  & .footer-legal {
-    @apply flex-auto m-auto;
+  & .footer-navigation {
+    columns: 2;
+    flex: 0 1 0%;
+    margin: auto auto auto 0;
+    @screen sm {
+      columns: unset;
+      flex: 1 1 auto;
+      margin: auto;
+
+    }
   }
 
   & .footer-legal {
-    @apply uppercase text-right;
+    position: absolute;
+    bottom: .25rem;
+    right: .25rem;
+    @screen sm {
+      right: 1rem;
+      bottom: 2rem;
+    }
+    @screen md {
+      flex: 0 1 auto;
+      margin: auto;
+      position: relative;
+      bottom: inherit;
+      right: inherit;
+    }
+    /* @apply text-right; */
   }
 }
 </style>
