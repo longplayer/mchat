e7aa8ba20a27e7152c2d639f9552fe0dc8740366
longplayer <bartaber@hotmail.com>
2022-01-26 21:06:07 +0100

    Deploy to gh-pages

--- src/helpers/bodyOverflow.js
+++
@@ -1,9 +0,0 @@
-function applyOverflowToBody(isAllowed) {
-  if (isAllowed) {
-    document.body.classList.add('pge__overflow--hidden')
-  } else {
-    document.body.classList.remove('pge__overflow--hidden')
-  }
-}
-
-export default applyOverflowToBody
